a1badc882a0b3d29c4aeaf4b90c156bd2ba444ed
Pedro Spido <pedrospido@hotmail.com>
2023-02-12 13:28:44 -0300

    Jogo da forca

---
+++ Jogo da forca/forca.h
@@ -0,0 +1,19 @@
+//
+// Created by Pedro Spido on 06/02/2023.
+//
+
+#ifndef JOGO_DA_FORCA_EXE_FORCA_H
+#define JOGO_DA_FORCA_EXE_FORCA_H
+
+#endif //JOGO_DA_FORCA_EXE_FORCA_H
+
+#define TAMANHO_PALAVRA 20;
+int acertou();
+int enforcou();
+void chuta();
+void desenhaforca();
+int jachutou(char letra);
+void escolhapalavra();
+void abertura();
+void adicionapalavra();
+int chuteserrados();
